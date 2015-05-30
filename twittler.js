336b55fccfc3b62418fdb33fe4439bf663936c9d
MichaelGofron <gofronm@yahoo.com>
2015-05-29 21:26:47 -0500

    Fixed simple bug where dates were in wrong order

--- twittler.js
+++ twittler.js
@@ -2,23 +2,22 @@
 
         // function now orders by wrong dates such that the latest are at the bottom
         // we want it to be the other way
-        var lastIndex = 0;
+
+        var index = 0;
         function viewNewTweets(){
-          var index = streams.home.length - 1;
-          var tempIndex = index;
-          while(index > lastIndex){
+          var lastIndex = streams.home.length - 1;
+          while(index < lastIndex){
             var tweet = streams.home[index];
             var $tweet = $('<div class="container-fluid tweet"></div>');
             $tweet.text('@' + tweet.user + ': ' + tweet.message
              + " " + tweet.created_at);
             $tweet.appendTo($body);
-            index -= 1;
+            index += 1;
           }
-          lastIndex = tempIndex;
           console.log("calling new Tweets");
         }
 
-        function addButton(){
+        function addRefreshButton(){
           var $button = $('<button id="refresh">Refresh</button>');
           $button.on("click",viewNewTweets);
           $button.prependTo($body);
@@ -28,6 +27,6 @@
         $body.html('');
 
         viewNewTweets();
-        addButton();
+        addRefreshButton();
       });
       
